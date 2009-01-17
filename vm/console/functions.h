b20e6ebfee34abc371f6ff78c6918abd98f86e35
Charles Childers <crc.platypus@gmail.com>
2009-01-17 07:58:41 -0500

    Faster building of console VM (from Derek Peschel)

--- vm/console/functions.h
+++ vm/console/functions.h
@@ -2,37 +2,37 @@
 #define _FUNCTIONS_H
 
 /* FILE: devices.c */
-int dev_getch();
-void dev_refresh();
-void dev_include(char *s);
-void dev_putch(int c);
-void dev_init(int level);
-void dev_cleanup();
+extern int dev_getch();
+extern void dev_refresh();
+extern void dev_include(char *s);
+extern void dev_putch(int c);
+extern void dev_init(int level);
+extern void dev_cleanup();
 
 /* FILE: disassemble.c */
-void display_instruction();
+extern void display_instruction();
 
 /* FILE: endian.c */
-void swapEndian();
+extern void swapEndian();
 
 /* FILE: initial_image.c */
-void initial_image();
+extern void initial_image();
 
 /* FILE: loader.c */
-int vm_load_image(char *image);
-int vm_save_image(char *image);
+extern int vm_load_image(char *image);
+extern int vm_save_image(char *image);
 
 /* FILE: ngaro.c */
-int main(int argc, char **argv);
+extern int main(int argc, char **argv);
 
 /* FILE: video.c */
-void drawpixel(int x, int color);
-int update_display(void *unused);
+extern void drawpixel(int x, int color);
+extern int update_display(void *unused);
 
 /* FILE: vm.c */
-void init_vm();
-void vm_process();
-void init_devices();
-void cleanup_devices();
+extern void init_vm();
+extern void vm_process();
+extern void init_devices();
+extern void cleanup_devices();
 
 #endif
