9a5558748c07b83faaab748e6f02321dd2647a74
Charles Childers <crc.platypus@gmail.com>
2009-06-06 16:34:05 -0400

    silence warnings in endian.c

--- vm/framebuffer/endian.c
+++ vm/framebuffer/endian.c
@@ -14,21 +14,18 @@
 
 extern VM_STATE vm;
 
-#define BIGENDIAN     0
-#define LITTLEENDIAN  1
-
 void swapEndian()
 {
-  int endian, x, a;
+  int x, a;
   int b1, b2, b3, b4;
 
   for(a = 0; a < IMAGE_SIZE; a++)
   {
     x = vm.image[a];
-    int b1 = (x >>  0) & 0xff;
-    int b2 = (x >>  8) & 0xff;
-    int b3 = (x >> 16) & 0xff;
-    int b4 = (x >> 24) & 0xff;
+    b1 = (x >>  0) & 0xff;
+    b2 = (x >>  8) & 0xff;
+    b3 = (x >> 16) & 0xff;
+    b4 = (x >> 24) & 0xff;
     vm.image[a] = b1 << 24 | b2 << 16 | b3 << 8 | b4 << 0;
   }
 }
