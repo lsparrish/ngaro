df3c3936fb7a17dd89608691a44e23548df0017c
Charles Childers <crc.platypus@gmail.com>
2009-06-06 15:42:23 -0400

    fixed endian conversion code in C implementations

--- vm/framebuffer/endian.c
+++ vm/framebuffer/endian.c
@@ -1,10 +1,6 @@
 /******************************************************
  * Ngaro
  *
- *|F|
- *|F| FILE: endian.c
- *|F|
- *
  * Written by Charles Childers, released into the public
  * domain
  ******************************************************/
@@ -21,25 +17,19 @@ extern VM_STATE vm;
 #define BIGENDIAN     0
 #define LITTLEENDIAN  1
 
-/* This is a slightly tricky bit of code to convert an image from one endian to the other. */
-/* It's only recieved minimal testing, so I hope it actually works... */
 void swapEndian()
 {
   int endian, x, a;
-  short int word = 0x0001;
-  char *byte = (char *) &word;
-  endian = (byte[0] ? LITTLEENDIAN : BIGENDIAN);
-
-  if (endian == LITTLEENDIAN)
-    printf("Converting to big endian\n");
-  if (endian == BIGENDIAN)
-    printf("Converting to little endian\n");
+  int b1, b2, b3, b4;
 
   for(a = 0; a < IMAGE_SIZE; a++)
   {
     x = vm.image[a];
-    vm.image[a] = (x>>24) | ((x<<8) & 0x00FF0000) | ((x>>8) & 0x0000FF00) | (x<<24);
+    int b1 = (x >>  0) & 0xff;
+    int b2 = (x >>  8) & 0xff;
+    int b3 = (x >> 16) & 0xff;
+    int b4 = (x >> 24) & 0xff;
+    vm.image[a] = b1 << 24 | b2 << 16 | b3 << 8 | b4 << 0;
   }
-  printf("Done!\n");
 }
 
