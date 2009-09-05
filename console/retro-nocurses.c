74ad8168417b22db59eb554bd98a058a079b8033
Charles Childers <crc.platypus@gmail.com>
2009-09-05 16:42:26 -0400

    merge ioctl and curses device backends; now selectable at compile time [ioctl is default]

--- console/retro-nocurses.c
+++
@@ -1,2 +0,0 @@
-//FLAGS -O3 -fomit-frame-pointer
-//USES devices_ioctl disassemble endian loader ngaro vm
--- console/retro.c
+++ console/retro.c
@@ -1,3 +1,2 @@
 //FLAGS -O3 -fomit-frame-pointer
-//LIBS ncurses
 //USES devices disassemble endian loader ngaro vm
